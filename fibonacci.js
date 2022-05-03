bbcfb7c526374c0993b098ae5444f9918a1a8570
Gerard Tolan <gerardtolan@Gerards-MacBook-Pro.local>
2022-05-03 18:38:55 +0200

    recursive wsingle tail call

--- fibonacci.js
+++ fibonacci.js
@@ -5,4 +5,11 @@ const fib = (n, memo = {}) => {
         memo[n] = fib(n-1, memo) + fib(n-2, memo);
         return memo[n];
 }
-console.log(fib(8))
+console.log(fib(8)) 
+
+const fibBest = (n, a = 0, b = 1) => {
+    if(n == 1) return b;
+    if(n == 0) return a;
+    return fibBest(n - 1, b, a + b)
+}
+console.log(fibBest(8)); 
