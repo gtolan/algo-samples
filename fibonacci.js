c7dc8ace7435f3defb405e8f66febde429890096
Gerard Tolan <tolangerard@gmail.com>
2022-05-03 18:57:41 +0200

    best fib comments

--- fibonacci.js
+++ fibonacci.js
@@ -7,9 +7,15 @@ const fib = (n, memo = {}) => {
 }
 console.log(fib(8)) 
 
+//first two numbers start at a:0,b:1
+//theseare added together over N iterations
+//
 const fibBest = (n, a = 0, b = 1) => {
-    if(n == 1) return b;
+    console.log(`n:${n},a:${a},b:${b}`)
+    if(n == 1) return b; //running total of adding a + b 
     if(n == 0) return a;
+    // n iterations are decremented - b is passed as a, -b is the sum of a + b
+    //b is the current fib number on N iterations - a is the previous fib
     return fibBest(n - 1, b, a + b)
 }
-console.log(fibBest(6)); 
+console.log(fibBest(8)); 
